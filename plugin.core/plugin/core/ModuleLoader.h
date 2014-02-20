b20ce43ce053a4e13b1ba7a90713bff0ec88c0a7
perandersson <dim.raven@gmail.com>
2014-02-20 20:28:18 +0100

    Added comments

--- plugin.core/plugin/core/ModuleLoader.h
+++ plugin.core/plugin/core/ModuleLoader.h
@@ -11,10 +11,29 @@ namespace plugin
 		class ModuleLoader
 		{
 		public:
+			//
+			// Load the supplied library and return a library handle for it
+			//
+			// @param fileName
+			//			The library filename
+			// @return A library handle; NULL if no library could be loaded
 			static LibraryHandle GetLibraryHandle(const char* fileName);
 
+			//
+			// Unload the supplied library
+			//
+			// @param library
+			//			The library
 			static void UnloadLibrary(LibraryHandle library);
 
+			//
+			// Load a function from the supplied library and return a library handle for it
+			//
+			// @param library
+			//			The library
+			// @param name
+			//			The function name
+			// @return A function if found; NULL if no function was found
 			template<typename T>
 			static T GetFunction(LibraryHandle library, const char* name) {
 				void* ptr = GetFunctionPtr(library, name);
